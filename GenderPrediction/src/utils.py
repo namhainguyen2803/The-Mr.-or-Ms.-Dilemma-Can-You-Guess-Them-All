dd260f15a2c751ef017b8b7bba13b9d792a617ce
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2023-05-23 23:37:35 +0700

    code for logreg, svm, truncatedSVD, evaluate metrics

---
+++ GenderPrediction/src/utils.py
@@ -0,0 +1,76 @@
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+
+# Models
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+# Visual purpose
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
+def evaluate(model_info, y_true, y_pred):
+    """
+    Evaluate the model: accuracy, f1 score, confusion matrix
+    """
+    print(Fore.LIGHTYELLOW_EX + model_info)
+
+    accuracy = accuracy_score(y_true, y_pred)
+    print(Fore.LIGHTBLUE_EX + "Accuracy:", round(accuracy, 4))
+
+    f1 = f1_score(y_true, y_pred)
+    print(Fore.LIGHTBLUE_EX + "F1-score:", round(f1, 4))
+
+    cm = confusion_matrix(y_true, y_pred)
+    print(Fore.LIGHTBLUE_EX + "Confusion matrix:\n", cm)
+
+    return accuracy, f1, cm
+
+def truncated_svd(X_train, X_test, random_state, n_components=100):
+    svd = TruncatedSVD(n_components=n_components, random_state=random_state)
+    svd.fit(X_train)
+    transformed_X_train = svd.transform(X_train)
+    transformed_X_test = svd.transform(X_test)
+    return transformed_X_train, transformed_X_test
+
+def logistic_regression(X_train, y_train, X_test, y_test, random_state, regularization="ridge", regularization_strength=1):
+    """
+    regularization_strength: inverse of alpha coefficient in regularization
+    Possible regularizations: 'lasso', 'ridge', None
+    """
+
+    if regularization == "lasso":
+        logreg = LogisticRegression(penalty="l1", solver="liblinear", C=regularization_strength, random_state=random_state)
+    elif regularization == "ridge":
+        logreg = LogisticRegression(penalty="l2", solver="lbfgs", C=regularization_strength, random_state=random_state)
+    else:
+        logreg = LogisticRegression(penalty=None, solver="lbfgs", C=regularization_strength, random_state=random_state)
+
+    logreg.fit(X_train, y_train)
+
+    # Making predictions on the test set
+    y_pred = logreg.predict(X_test)
+
+    # Evaluate
+    accuracy, f1, cm = evaluate("Logistic Regression - Seed: " + str(random_state) + " - Regularization: " + str(regularization) + " - Regularization_strength: " + str(regularization_strength), y_test, y_pred)
+
+    return accuracy, f1, cm
+
+def support_vector_machine(X_train, y_train, X_test, y_test, random_state, misclass_penalty=1.0, kernel='rbf'):
+    """
+    misclass_penalty: penalty for misclassifying a data point, smaller ~ large margin
+    Possible kernels: 'linear', 'poly' (degree 3), 'rbf', 'sigmoid'
+    """
+    
+    svm = SVC(C=misclass_penalty, kernel=kernel, random_state=random_state)
+
+    svm.fit(X_train, y_train)
+
+    # Making predictions on the test set
+    y_pred = svm.predict(X_test)
+
+    # Evaluate
+    accuracy, f1, cm = evaluate("Support Vector Machine - Seed: " + str(random_state) + " - Misclass_penalty: " + str(misclass_penalty) + " - Kernel: " + str(kernel), y_test, y_pred)
+
+    return accuracy, f1, cm
