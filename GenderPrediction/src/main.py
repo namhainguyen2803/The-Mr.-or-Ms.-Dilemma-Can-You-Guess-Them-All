d32f639f8d6e944f320eb0313f86facd612ad4b8
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2023-05-23 23:38:12 +0700

    create interface for testing models

--- GenderPrediction/src/main.py
+++ GenderPrediction/src/main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+# Basic imports
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Custom imports
+import utils
+import tf_idf
 
+def main():
+    # Set random seed
+    RANDOM_STATE = 1989
+    
+    parent_directory = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    file_path = os.path.join(parent_directory, "dataset/name_full.csv")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # Import full data and split train-test set
+    data = pd.read_csv(file_path)
+    X = data["Full_Name"].values
+    y = data["Gender"].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM_STATE, stratify=y)
+    
+    # Vectorize/Embed via TF-IDF
+    TF_IDF = tf_idf.Compute_TF_IDF(X_train)
+    tfidf_X_train = TF_IDF.compute_tf_idf()
+    tfidf_X_test = TF_IDF.compute_tf_idf_for_test(X_test)
 
+    # Reduce dimensionality via TruncatedSVD
+    tfidf_X_train, tfidf_X_test = utils.truncated_svd(tfidf_X_train, tfidf_X_test, random_state=RANDOM_STATE)
+    
+    # Apply different models
+    ### utils.logistic_regression(tfidf_X_train, y_train, tfidf_X_test, y_test, random_state=RANDOM_STATE, regularization="ridge")
+    utils.support_vector_machine(tfidf_X_train, y_train, tfidf_X_test, y_test, random_state=RANDOM_STATE, kernel="sigmoid")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
