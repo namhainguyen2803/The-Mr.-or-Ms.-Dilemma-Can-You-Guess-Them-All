c1f8f003e0a531c35d01926ff879fa669df4d509
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2023-06-08 00:08:21 +0700

    changed main interface

--- GenderPrediction/src/main.py
+++ GenderPrediction/src/main.py
@@ -1,16 +1,18 @@
 # Basic imports
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 # Custom imports
-import utils
+from utils import *
 import tf_idf
+from metrics import Metric
 
 def main():
     # Set random seed
-    RANDOM_STATE = 2023
-    
+    RANDOM_STATE = 1989
+
     parent_directory = os.path.abspath(os.path.join(os.getcwd(), ".."))
     file_path = os.path.join(parent_directory, "dataset/name_full.csv")
 
@@ -26,27 +28,25 @@ def main():
     tfidf_X_test = TF_IDF.compute_tf_idf_for_test(X_test)
 
     # Reduce dimensionality via TruncatedSVD
-    tfidf_X_train, tfidf_X_test = utils.truncated_svd(tfidf_X_train, tfidf_X_test, random_state=RANDOM_STATE)
+    tfidf_X_train, tfidf_X_test = truncated_svd(tfidf_X_train, tfidf_X_test, random_state=RANDOM_STATE)
     
     # Apply different models
-    """
-    utils.logistic_regression(tfidf_X_train, y_train, tfidf_X_test, y_test, 
-                              random_state=RANDOM_STATE, 
-                              regularization="ridge", 
-                              regularization_strength=1)
-    """
-    """
-    utils.support_vector_machine(tfidf_X_train, y_train, tfidf_X_test, y_test, 
-                                 random_state=RANDOM_STATE, 
-                                 misclass_penalty=5.0,
-                                 kernel="linear",
-                                 )
-    """
-    utils.k_nearest_neighbors(tfidf_X_train, y_train, tfidf_X_test, y_test,
-                              n_neighbors=15,
-                              neighbor_weight="distance",
-                              p=2,
-                              metric="cosine")
+    ## LOGREG
+    
+    LOGREG_PARAM_GRID = {
+        'penalty': [None, "l1", "l2"],
+        'C': [0.01, 0.03, 0.1, 0.3, 1, 3]
+    }
+    logreg = MyLogisticRegression(random_state=RANDOM_STATE,
+                                grid_search=True, scoring="accuracy", param_grid=LOGREG_PARAM_GRID, cv=5)
+    logreg.fit(tfidf_X_train, y_train)
+    y_pred = logreg.predict(tfidf_X_test)
+    y_proba = logreg.predict_proba(tfidf_X_test)
+    logreg_metric = Metric(y_test, y_pred, y_proba[:, 1])
+    
+    pr_disp = logreg_metric.getRocCurveClass1()
+    pr_disp.plot()
+    plt.show()
 
 if __name__ == "__main__":
     main()
