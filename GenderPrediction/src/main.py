e2db7dd4e6f57d6739670b9199bf86bf86d06012
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2023-06-08 10:30:27 +0700

    changed main interface

--- GenderPrediction/src/main.py
+++ GenderPrediction/src/main.py
@@ -1,39 +1,21 @@
 # Basic imports
-import os
-import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
 # Custom imports
-from utils import *
-import tf_idf
-from metrics import Metric
-from truncated_svd import truncated_svd
+from models import *
+from data_reader import *
 
 def main():
     # Set random seed
     RANDOM_STATE = 1989
 
-    parent_directory = os.path.abspath(os.path.join(os.getcwd(), ".."))
-    file_path = os.path.join(parent_directory, "dataset/name_full.csv")
-
-    # Import full data and split train-test set
-    data = pd.read_csv(file_path)
-    X = data["Full_Name"].values
-    y = data["Gender"].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM_STATE, stratify=y)
-    
-    # Vectorize/Embed via TF-IDF
-    TF_IDF = tf_idf.Compute_TF_IDF(X_train)
-    tfidf_X_train = TF_IDF.compute_tf_idf()
-    tfidf_X_test = TF_IDF.compute_tf_idf_for_test(X_test)
-
-    # Reduce dimensionality via TruncatedSVD
-    tfidf_X_train, tfidf_X_test = truncated_svd(tfidf_X_train, tfidf_X_test, random_state=RANDOM_STATE)
+    # Read data
+    tfidf_X_train, tfidf_X_test, y_train, y_test = data_reader(random_state=RANDOM_STATE, truncate=True)
     
     # Apply different models
-    ## LOGREG
     
+        ## LOGREG
+    """
     LOGREG_PARAM_GRID = {
         'penalty': [None, "l1", "l2"],
         'C': [0.01, 0.03, 0.1, 0.3, 1, 3]
@@ -41,6 +23,29 @@ def main():
     logreg = MyLogisticRegression(tfidf_X_train, tfidf_X_test, y_train, y_test, random_state=RANDOM_STATE,
                                 grid_search=True, scoring="accuracy", param_grid=LOGREG_PARAM_GRID, cv=5)
     logreg.evaluate()
+    """
+        ## SVM
+    """
+    SVM_PARAM_GRID = {
+        'C': [0.01, 0.03, 0.1, 0.3, 1, 3],
+        'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
+    }
+    svm = MySupportVectorMachine(tfidf_X_train, tfidf_X_test, y_train, y_test, random_state=RANDOM_STATE,
+                                 grid_search=True, scoring="accuracy", param_grid=SVM_PARAM_GRID, cv=5)
+    svm.evaluate()
+    """
+        ## KNN
+    
+    KNN_PARAM_GRID = {
+        'n_neighbors': [7, 13, 19, 25],
+        'weights': ['uniform', 'distance'],
+        'p': [1, 2, 3, 4, 5],
+        'metric': ['cityblock', 'cosine', 'euclidean', 'minkowski']
+    }
+    knn = MyKNearestNeighbor(tfidf_X_train, tfidf_X_test, y_train, y_test,
+                             grid_search=True, scoring="accuracy", param_grid=KNN_PARAM_GRID, cv=5)
+    knn.evaluate()
+    
 
 if __name__ == "__main__":
     main()
